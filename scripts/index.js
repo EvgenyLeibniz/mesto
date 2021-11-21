9641fd16d125c1025c64dcc4447a89d6ade0658c
Vasiliy Shcherbakov <athazagoraphobia@tutanota.com>
2021-11-21 22:20:01 +0500

    fixed function popupSave

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,10 @@ const editButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const popupCloseButton = popup.querySelector(".popup__close-button");
 const popupSubmitButton = popup.querySelector(".popup__submit-button");
-let nameUser = document.querySelector(".profile__name");
-let nameInfo = document.querySelector(".profile__name-info");
-let formName = popup.querySelector(".popup__input_name");
-let formInfo = popup.querySelector(".popup__input_info");
+const nameUser = document.querySelector(".profile__name");
+const nameInfo = document.querySelector(".profile__name-info");
+let formName = popup.querySelector(".popup__input_type_name");
+let formInfo = popup.querySelector(".popup__input_type_info");
 
 function openPopup() {
   popup.classList.add("popup_opened");
@@ -20,25 +20,14 @@ function closePopup() {
 
 function savePopup(evt) {
   evt.preventDefault();
-
-  if (formName.value.length >= 2 && formInfo.value.length >= 2) {
-    popup.classList.remove("popup_opened");
     nameUser.textContent = formName.value;
     nameInfo.textContent = formInfo.value;
-  } else {
-    return;
-  }
+    closePopup();
 }
 
 editButton.addEventListener("click", openPopup);
 popupCloseButton.addEventListener("click", closePopup);
 formElement.addEventListener("submit", savePopup);
 
-// Зравствуйте, Алексей! Большое спасибо Вам за хорошее и понятное ревью, почерпнул для себя много интересных моментов, которые вряд ли узнал бы без встречи с Вами :)
-
-// Я не смог до конца разобраться стоит ли в блоке cards элементам card присваивать семантическую обертку article, если до этого сделал из них список..
-
-// В js файле постарался изменить проверку по методике, которую вы посоветовали, но до конца не разобрался когда стоит использовать метод popup.classList.remove("popup_opened") в блоке функции
-// savePopup() - в условии if или же после выполнения обоих условий (поэтому поставил в условии запуска функции, но был бы признателен Вам за совет).
-
-// В остальном постарался по максимум учесть замечания
+// Спасибо за добавленные материалы для ознакомления, я понимаю, что показываю слабый уровень
+// подготовки и занимаю ваше время, благодарю за терпение))
